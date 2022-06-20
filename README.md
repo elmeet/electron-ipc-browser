55bc17fa374739f57608301c9babff24fd7fc590
徐文强 <wenqiang_xu@shannonai.com>
2022-06-20 20:11:54 +0800

    electron browser mock

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # electron-ipc-browser
+
 运行在浏览器下, 模拟 electron 渲染进程 ipc 通信
---
+++ src/index.ts
@@ -0,0 +1,97 @@
+const isString = (v: unknown): v is string => typeof v === "string";
+const isFunction = (v: unknown): v is (...args: unknown[]) => unknown =>
+  typeof v === "function";
+
+export type RendererToMainData = {
+  id: string;
+  data: unknown;
+  name: string;
+};
+
+export type MainToRendererData = {
+  data: unknown;
+  name: string;
+};
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+type Parameter<T extends (args: any) => any> = T extends (args: infer P) => any
+  ? P
+  : never;
+
+type FunctionPromiseMayBe<T extends (...args: any) => any> = T extends (
+  args: any
+) => Promise<any>
+  ? T
+  : T extends (args: infer P) => infer R
+  ? ((args: P) => Promise<R>) | T
+  : T;
+
+type PromiseResolveType<T> = T extends Promise<infer R> ? R : T;
+type ReturnResolveType<T extends (...args: any) => any> = T extends (
+  ...args: any
+) => infer R
+  ? PromiseResolveType<R>
+  : any;
+
+// export interface MainHubOptions {
+//   onReceiveBeforeEach?: (arg: RendererToMainData) => void;
+//   onReplyBeforeEach?: (arg: RendererToMainData) => void;
+//   onSendBeforeEach?: (arg: MainToRendererData) => void;
+// }
+
+let singleMainHub: unknown | null = null;
+
+export function useMainHub<
+  RendererToMain extends Record<string, (args: any) => any>,
+  MainToRenderer extends Record<string, unknown>
+>() {
+  if (singleMainHub) {
+    return singleMainHub as typeof hub;
+  }
+
+  const _all: Map<string, Function> = new Map();
+
+  type MainKey = keyof RendererToMain;
+  type MainHandler<K extends MainKey> = FunctionPromiseMayBe<RendererToMain[K]>;
+
+  // type RendererKey = keyof MainToRenderer;
+  // type RendererHandler<K extends RendererKey> = MainToRenderer[K];
+
+  const hub = {
+    on<P extends MainKey>(name: P, handler: MainHandler<P>) {
+      if (!isString(name)) {
+        throw new TypeError("[electron-ipc-hub main] param name is not string");
+      }
+      if (!isFunction(handler)) {
+        throw new TypeError(
+          "[electron-ipc-hub main] param handler is not function"
+        );
+      }
+      _all.set(name, handler);
+    },
+    off<P extends MainKey>(name: P) {
+      if (!isString(name)) {
+        throw new TypeError("param name is not function");
+      }
+      if (_all.has(name)) {
+        _all.delete(name);
+      }
+    },
+    async emit<P extends MainKey>(
+      name: P,
+      data: Parameter<MainHandler<P>>
+    ): Promise<ReturnResolveType<MainHandler<P>>> {
+      const handler = _all.get(name as string);
+      if (handler) {
+        return Promise.resolve(handler(data));
+      } else {
+        return Promise.reject(
+          new Error(`[electron-ipc-hub main] main process not listen ${name as string}`)
+        );
+      }
+    },
+  };
+
+  singleMainHub = hub;
+  return hub;
+}
